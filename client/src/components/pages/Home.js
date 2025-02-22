915c57310fb3900406591e80f55bbffa0e1dbb14
HelloStudentWorld <deguzman10student@gmail.com>
2025-02-21 23:24:22 -0800

    style: separate login and register buttons with vertical layout

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './Home.css';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCategories } from '../../features/categories/categoriesSlice';
